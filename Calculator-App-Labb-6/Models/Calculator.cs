e07a44bb2f6bede0652487e8499b23f9f74fb3c5
Christian Rapp <christian.rapp@edu.varberg.se>
2024-05-29 10:59:31 +0200

    Add project files.

---
+++ Calculator-App-Labb-6/Models/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_App_Labb_6.Models
+{
+    public class Calculator
+    {
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Multiply(double a, double b) => a * b;
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            return a / b;
+        }
+    }
+}
---
+++ Calculator-App-Labb-6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator_App_Labb_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
